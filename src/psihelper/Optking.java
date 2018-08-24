cb92d3b3242e4a8b14ec8fd480c88ea4e337106a
marpat <marcelpatek@yahoo.com>
2018-08-23 19:38:53 -0700

    merged constr and extension

---
+++ src/psihelper/Optking.java
@@ -0,0 +1,17 @@
+/*
+ * Optking.java, part of the PsiHelper project
+ * http://chemgplus.blogspot.com/
+ */
+
+package psihelper;
+
+/**
+ * Optking.java (UTF-8)
+ *
+ * Aug 23, 2018
+ * @author Marcel Patek <chemgplus at gmail.com>
+ */
+
+public class Optking extends FXMLDocumentController  {
+
+}
